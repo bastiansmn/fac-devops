9e07ce870681f0f04cde7a0b5a70128c48f64741
Bastian Somon <bastian.somon@gmail.com>
2024-06-21 13:28:03 +0200

    Fix: CI

--- backend-api/src/test/java/fr/takima/training/sampleapplication/IT/DepartmentControllerTestIT.java
+++ backend-api/src/test/java/fr/takima/training/sampleapplication/IT/DepartmentControllerTestIT.java
@@ -1,4 +1,4 @@
-tapackage fr.takima.training.sampleapplication.IT;
+package fr.takima.training.sampleapplication.IT;
 
 import fr.takima.training.simpleapi.SimpleApiApplication;
 import org.junit.jupiter.api.Test;
